22471b4477ce2a2cb440e5fa2bb60274c009420a
Vladyslav84 <fakingbrutal@gmail.com>
2021-05-19 18:04:27 +0300

    Добавив ключ

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import handlabars from '../node_modules/handlebars';
+import axios from '../node_modules/axios';
+
+const PIXABAY_Key = '21694115-487a2c793b7208539d5182bab';
+
+console.log(axios)
