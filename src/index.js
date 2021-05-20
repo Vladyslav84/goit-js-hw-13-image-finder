fbf1da1308f6676cbc7a4625d43dfd118c0a0c92
Vladyslav84 <fakingbrutal@gmail.com>
2021-05-20 16:47:10 +0300

    Переробив на класи.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import './sass/main.scss';
-import handlabars from '../node_modules/handlebars';
-import axios from '../node_modules/axios';
+// import handlabars from '../node_modules/handlebars';
+// import axios from '../node_modules/axios';
 import _debounce from '../node_modules/lodash.debounce';
 import galleryTml from './templates/galleryTml.hbs';
-const BASE_URL = 'https://pixabay.com/api/';
-const PIXABAY_KEY = '21694115-487a2c793b7208539d5182bab';
+import ApiService from './js/apiService';
+// const BASE_URL = 'https://pixabay.com/api/';
+// const PIXABAY_KEY = '21694115-487a2c793b7208539d5182bab';
 const galleryEl = document.querySelector('.gallery');
 const LoadMoreBtnEl = document.querySelector('.LoadMoreBtn');
 const input = document.querySelector('.input');
+const apiService = new ApiService();
 
-let perPage = 12;
-let pageNum = 1;
-let searhImg = '';
-console.log(axios);
+// let perPage = 12;
+// let pageNum = 1;
+// let searhImg = '';
+console.log(apiService);
 
 LoadMoreBtnEl.addEventListener('click', handleloadMore);
-input.addEventListener('input', _debounce(() => {
+// input.addEventListener('input', _debounce(() => {
 
-    searhImg = input.value;
+//     searhImg = input.value;
 
-    getImg(searhImg);
+//     getImg(searhImg);
 
-}, 500),);
+// }, 500),);
+input.addEventListener('input', _debounce(getImg, 500));
 
 // console.log(searhImg);
 
@@ -31,46 +34,19 @@ input.addEventListener('input', _debounce(() => {
 
 // }
 
-function getImg() {
+function getImg(evt) {
+    evt.preventDefault();
+
+    apiService.query = evt.target.value;
+
+    apiService.getImg();
+
+console.log(apiService.query)
     
-    return axios.get(`${BASE_URL}?key=${ PIXABAY_KEY }&q=${searhImg}&image_type=photo&page=${ pageNum }&per_page=${ perPage }&image_type=photo&orientation=horizontal&`)
-        .then(function (response) {
-
-            const imgObject = response.data.hits;
-
-            const renderPage = galleryTml(imgObject);
-
-            renderGallary(renderPage);
-
-             resetRenderPage();
-
-            LoadMoreBtnEl.classList.toggle('opacity');
-
-        }).catch(error => {
-            console.log(error);
-
-        })
 };
-function handleloadMore (imgName){
-    console.log(imgName);
-    perPage += 12;
-    pageNum += 1;
 
-    return axios.get(`https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${imgName}&image_type=photo&page=${pageNum}&per_page=${perPage}&image_type=photo&orientation=horizontal&`)
-        .then(function (response) {
- 
-            const imgObject = response.data.hits;
-
-            const renderPage = galleryTml(imgObject);
-
-            renderLoadMore(renderPage);
-
-            console.log(imgObject);
-
-        }).catch(error => {
-            console.log(error);
-            
-        })
+function handleloadMore (){
+ apiService.getImg();
 };
 
 console.log()
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,11 +1,35 @@
-const BASE_URL = 'https://pixabay.com/api/';
+import axios from '../../node_modules/axios';
+// import galleryTml from '../templates/galleryTml.hbs';
+export default class ApiService {
+    constructor() {
+        this.imgName = '';
+        this.pageNum = 1;
+        this.perPage = 12;
+        this.PIXABAY_KEY = '21694115-487a2c793b7208539d5182bab';
+    }
+    getImg() {
+        return axios.get(`https://pixabay.com/api/?key=${this.PIXABAY_KEY}&q=${this.imgName}&image_type=photo&page=${this.pageNum}&per_page=${this.perPage}&image_type=photo&orientation=horizontal&`)
+            .then(function (response) {
+ 
+                return response.data.hits;
 
-function fetchPokemon(pokemonId) {
-    return fetch(`${ BASE_URL }/pokemon/${ pokemonId }`).then(response =>
-        response.json(),
-    );
-}
+                // const renderPage = galleryTml(imgObject);
 
-export default { fetchPokemon };
-
-'BASE_URL?image_type=photo&orientation=horizontal&q=${searhImg}&page=${pageNum}&per_page=${perPage}&key=PIXABAY_KEY '
+                // renderLoadMore(renderPage);
+            })
+            .then(data => {
+                this.pageNum += 1;
+                this.perPage += 12;
+            })
+            .catch(error => {
+            console.log(error);
+            
+        })
+    }
+    get query() {
+        return this.imgName;
+    };
+    set query(newImgName) {
+        this.imgName = newImgName;
+    }
+};
